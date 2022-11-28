62711dda7f7c9b185b707359923e259f6e770dd0
luiserrador <luisppserrador@gmail.com>
2022-11-28 19:05:25 +0000

    train.py and test_train.py done

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,19 +1,60 @@
-import unittest
+import os
+import shutil
+
+import tensorflow as tf
 from utils.layers_func import create_unet3d
-from utils.train import *
+from utils.train import Trainer
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Comment to use GPU
 
 
 def testTrainer():
-    model = create_unet3d(input_shape=[128, 128, 128, 2],
-                          n_convs=2,
-                          n_filters=[8, 16, 32, 64],
+    """Testing Trainer Class"""
+
+    model_dir = 'tf_ckpt'
+
+    if os.path.exists(model_dir):
+        shutil.rmtree(model_dir)
+
+    model = create_unet3d(input_shape=[32, 32, 32, 1],
+                          n_convs=1,
+                          n_filters=[8, 16],
                           ksize=[3, 3, 3],
                           padding='same',
                           pooling='avg',
                           norm='batch_norm',
-                          dropout=[0.25, 0.5, 0.5],
+                          dropout=[0],
                           upsampling=True,
                           activation='relu',
-                          depth=4)
+                          depth=2)
 
-    trainer = Trainer(model, tf.keras.optimizers.Adam, 1e-3, 'tf_ckpt')
+    trainer = Trainer(model, tf.keras.optimizers.Adam, 1e-3, model_dir)
+
+    train_data = tf.data.Dataset.from_tensor_slices((tf.random.uniform([10, 32, 32, 32, 1]),
+                                                     tf.random.uniform([10, 32, 32, 32, 1])))
+
+    valid_data = tf.data.Dataset.from_tensor_slices((tf.random.uniform([10, 32, 32, 32, 1]),
+                                                     tf.random.uniform([10, 32, 32, 32, 1])))
+
+    train_ds = tf.distribute.get_strategy().experimental_distribute_dataset(train_data.batch(1).repeat())
+    valid_ds = tf.distribute.get_strategy().experimental_distribute_dataset(valid_data.batch(1).repeat())
+
+    loss_fn = tf.keras.losses.binary_crossentropy
+    accuracy_fn = tf.keras.metrics.binary_accuracy
+
+    print('\nTrain 2 epochs and stop')
+
+    trainer.train(train_ds=train_ds, valid_ds=valid_ds, train_size=10, validation_size=10, loss_fn=loss_fn,
+                  accuracy_fn=accuracy_fn, BATCH_SIZE=1, EPOCHS=2, save_step=1)
+
+    print('\nResume training from 2nd')
+
+    trainer.train(train_ds=train_ds, valid_ds=valid_ds, train_size=10, validation_size=10, loss_fn=loss_fn,
+                  accuracy_fn=accuracy_fn, BATCH_SIZE=1, EPOCHS=4, save_step=1)
+
+    print('\nTry to resume training when achieved number epochs')
+
+    trainer.train(train_ds=train_ds, valid_ds=valid_ds, train_size=10, validation_size=10, loss_fn=loss_fn,
+                  accuracy_fn=accuracy_fn, BATCH_SIZE=1, EPOCHS=4, save_step=1)
+
+    shutil.rmtree(model_dir)
--- utils/train.py
+++ utils/train.py
@@ -1,11 +1,16 @@
 import os.path
+import time
+import os
 
 import tensorflow as tf
 import numpy as np
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Comment to use GPU
+
 
 class Trainer:
     def __init__(self, model, optimizer, learning_rate, model_dir):
+
         self.model = model
 
         with tf.distribute.get_strategy().scope():
@@ -17,23 +22,101 @@ class Trainer:
             self.optimizer = optimizer(learning_rate=learning_rate)
 
             ckpt = tf.train.Checkpoint(optimizer=self.optimizer, net=self.model)
-            manager = tf.train.CheckpointManager(ckpt, model_dir, max_to_keep=3)
-            ckpt.restore(manager.latest_checkpoint)
+            self.manager = tf.train.CheckpointManager(ckpt, model_dir, max_to_keep=3)
+            ckpt.restore(self.manager.latest_checkpoint)
 
-        if manager.latest_checkpoint:
-            print("Restored from {}".format(manager.latest_checkpoint))
-            self.step = np.load(os.path.join(model_dir, "step_scratch.npy"))
+        check_dir = os.path.exists(model_dir)
+        if not check_dir:
+            os.makedirs(model_dir)
+
+        self.step_dir = os.path.join(model_dir, "step.npy")
+        self.model_dir = model_dir
+
+        if self.manager.latest_checkpoint:
+            print("Restored from {}".format(self.manager.latest_checkpoint))
+            self.step = np.load(self.step_dir)
         else:
             print("Initializing from scratch.")
             self.step = 0
+
+    def train(self, train_ds, valid_ds, train_size, validation_size, loss_fn, accuracy_fn, BATCH_SIZE, EPOCHS, save_step=1):
+
+        self.EPOCHS = EPOCHS
+
+        self.loss_fn = loss_fn
+        self.accuracy_fn = accuracy_fn
+
+        self.STEPS_PER_CALL = STEPS_PER_EPOCH = train_size // BATCH_SIZE
+        self.VALIDATION_STEPS_PER_CALL = validation_size // BATCH_SIZE
+        self.epoch = self.step // STEPS_PER_EPOCH
+        epoch_steps = 0
+        epoch_start_time = time.time()
+
+        history = {'loss': [], 'val_loss': [], 'acc': [], 'val_acc': []}
+
+        train_data_iter = iter(train_ds)
+        valid_data_iter = iter(valid_ds)
+
+        if self.epoch < self.EPOCHS:
+
+            while True:
+                # run training step
+                print('\nEPOCH {:d}/{:d}'.format(self.epoch + 1, self.EPOCHS))
+                print('Starting Training')
+                self.train_step(train_data_iter)
+                epoch_steps += self.STEPS_PER_CALL
+                self.step += self.STEPS_PER_CALL
+                print(epoch_steps, '/', STEPS_PER_EPOCH)
+
+                # validation run at the end of each epoch
+                if (self.step // STEPS_PER_EPOCH) > self.epoch:
+                    print('Starting Validation')
+                    # validation run
+                    valid_epoch_steps = 0
+                    self.valid_step(valid_data_iter)
+                    valid_epoch_steps += self.VALIDATION_STEPS_PER_CALL
+
+                    # compute metrics
+                    history['acc'].append(self.train_accuracy.result().numpy() / (BATCH_SIZE * epoch_steps))
+                    history['val_acc'].append(self.valid_accuracy.result().numpy() / (BATCH_SIZE * valid_epoch_steps))
+                    history['loss'].append(self.train_loss.result().numpy() / (BATCH_SIZE * epoch_steps))
+                    history['val_loss'].append(self.valid_loss.result().numpy() / (BATCH_SIZE * valid_epoch_steps))
+
+                    # report metrics
+                    epoch_time = time.time() - epoch_start_time
+                    print('time: {:0.1f}s'.format(epoch_time),
+                          'loss: {:0.4f}'.format(history['loss'][-1]),
+                          'acc: {:0.4f}'.format(history['acc'][-1]),
+                          'val_loss: {:0.4f}'.format(history['val_loss'][-1]),
+                          'val_acc: {:0.4f}'.format(history['val_acc'][-1]))
+
+                    print('Saving Model')
+                    if save_step and self.epoch % save_step == 0:
+                        model_path = (os.path.join(self.model_dir, 'model_epoch_%s.h5' % (self.epoch + 1)))
+                    self.model.save(model_path)
+                    self.manager.save()
+                    np.save(self.step_dir, self.step)
+
+                    # set up next epoch
+                    self.epoch = self.step // STEPS_PER_EPOCH
+                    epoch_steps = 0
+                    epoch_start_time = time.time()
+                    self.train_accuracy.reset_states()
+                    self.valid_accuracy.reset_states()
+                    self.valid_loss.reset_states()
+                    self.train_loss.reset_states()
+                    if self.epoch >= self.EPOCHS:
+                        print('Training done, {} epoch'.format(self.epoch))
+                        break
+        else:
+            print('\nAlready trained!')
 
     @tf.function
     def train_step(self, data_iter):
         def train_step_fn(images, labels):
             with tf.GradientTape() as tape:
                 probabilities = self.model(images, training=True)
-                loss = self.loss_fn(tf.cast(labels, tf.float32), probabilities, epoch=tf.cast(self.epoch, tf.float32),
-                                    EPOCHS=tf.cast(self.EPOCHS, tf.float32))
+                loss = self.loss_fn(tf.cast(labels, tf.float32), probabilities)
             grads = tape.gradient(loss, self.model.trainable_variables)
             self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
             accuracy = self.accuracy_fn(tf.cast(labels, tf.float32), probabilities)
@@ -48,8 +131,7 @@ class Trainer:
     def valid_step(self, data_iter):
         def valid_step_fn(images, labels):
             probabilities = self.model(images, training=False)
-            loss = self.loss_fn(tf.cast(labels, tf.float32), probabilities, epoch=tf.cast(self.epoch, tf.float32),
-                                EPOCHS=tf.cast(self.EPOCHS, tf.float32))
+            loss = self.loss_fn(tf.cast(labels, tf.float32), probabilities)
             accuracy = self.accuracy_fn(tf.cast(labels, tf.float32), probabilities)
             self.valid_accuracy.update_state(accuracy)
             self.valid_loss.update_state(loss)
