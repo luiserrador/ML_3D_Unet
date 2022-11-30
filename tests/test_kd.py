9e20f358bb4eac89cc471c0a961c4b0c3ca08dba
luiserrador <luisppserrador@gmail.com>
2022-11-30 14:19:12 +0000

    new mock data, new distillation loss

--- tests/test_kd.py
+++ tests/test_kd.py
@@ -58,12 +58,31 @@ def testKD():
         rand_x2 = np.random.randint(0, 64)
         rand_y2 = np.random.randint(0, 64)
         circle_data2 = (xx - rand_x2) ** 2 + (yy - rand_y2) ** 2
-        circle_data = np.random.uniform(0, 1, [64, 64]) * (
-                    np.logical_and(circle_data1 < 150, circle_data1 > 0, dtype=np.float) + np.logical_and(
-                circle_data2 < 100, circle_data2 > 0, dtype=np.float))
+        rand_x3 = np.random.randint(0, 64)
+        rand_y3 = np.random.randint(0, 64)
+        circle_data3 = (xx - rand_x3) ** 2 + (yy - rand_y3) ** 2
+        rand_x4 = np.random.randint(0, 64)
+        rand_y4 = np.random.randint(0, 64)
+        circle_data4 = (xx - rand_x4) ** 2 + (yy - rand_y4) ** 2
+        rand_x5 = np.random.randint(0, 64)
+        rand_y5 = np.random.randint(0, 64)
+        circle_data5 = (xx - rand_x5) ** 2 + (yy - rand_y5) ** 2
+        rand_x6 = np.random.randint(0, 64)
+        rand_y6 = np.random.randint(0, 64)
+        circle_data6 = (xx - rand_x6) ** 2 + (yy - rand_y6) ** 2
+        rand_x7 = np.random.randint(0, 64)
+        rand_y7 = np.random.randint(0, 64)
+        circle_data7 = (xx - rand_x7) ** 2 + (yy - rand_y7) ** 2
+        circle_data = np.random.uniform(0, 1, [64, 64]) * 15 * (np.logical_and(circle_data1 < 100, circle_data1 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 10 * (np.logical_and(circle_data2 < 150, circle_data2 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 5 * (np.logical_and(circle_data3 < 75, circle_data3 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 8 * (np.logical_and(circle_data4 < 125, circle_data4 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 10 * (np.logical_and(circle_data5 < 200, circle_data5 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 6 * (np.logical_and(circle_data6 < 250, circle_data6 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 3 * (np.logical_and(circle_data7 < 175, circle_data7 > 0, dtype=np.float))
         training_data[j, :, :, :, 0] = np.stack((circle_data,) * 64, axis=0)
-        circle_data = np.logical_and(circle_data1 < 200, circle_data1 > 0, dtype=np.float) + np.logical_and(
-            circle_data2 < 250, circle_data2 > 0, dtype=np.float)
+        circle_data = np.logical_and(circle_data1 < 100, circle_data1 > 0, dtype=np.float) + np.logical_and(
+            circle_data4 < 125, circle_data4 > 0, dtype=np.float)
         training_mask[j, :, :, :, 0] = np.stack((circle_data,) * 64, axis=0)
 
     for j in range(10):
@@ -74,12 +93,31 @@ def testKD():
         rand_x2 = np.random.randint(0, 64)
         rand_y2 = np.random.randint(0, 64)
         circle_data2 = (xx - rand_x2) ** 2 + (yy - rand_y2) ** 2
-        circle_data = np.random.uniform(0, 1, [64, 64]) * (
-                    np.logical_and(circle_data1 < 150, circle_data1 > 0, dtype=np.float) + np.logical_and(
-                circle_data2 < 100, circle_data2 > 0, dtype=np.float))
+        rand_x3 = np.random.randint(0, 64)
+        rand_y3 = np.random.randint(0, 64)
+        circle_data3 = (xx - rand_x3) ** 2 + (yy - rand_y3) ** 2
+        rand_x4 = np.random.randint(0, 64)
+        rand_y4 = np.random.randint(0, 64)
+        circle_data4 = (xx - rand_x4) ** 2 + (yy - rand_y4) ** 2
+        rand_x5 = np.random.randint(0, 64)
+        rand_y5 = np.random.randint(0, 64)
+        circle_data5 = (xx - rand_x5) ** 2 + (yy - rand_y5) ** 2
+        rand_x6 = np.random.randint(0, 64)
+        rand_y6 = np.random.randint(0, 64)
+        circle_data6 = (xx - rand_x6) ** 2 + (yy - rand_y6) ** 2
+        rand_x7 = np.random.randint(0, 64)
+        rand_y7 = np.random.randint(0, 64)
+        circle_data7 = (xx - rand_x7) ** 2 + (yy - rand_y7) ** 2
+        circle_data = np.random.uniform(0, 1, [64, 64]) * 15 * (np.logical_and(circle_data1 < 100, circle_data1 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 10 * (np.logical_and(circle_data2 < 150, circle_data2 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 5 * (np.logical_and(circle_data3 < 75, circle_data3 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 8 * (np.logical_and(circle_data4 < 125, circle_data4 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 12 * (np.logical_and(circle_data5 < 200, circle_data5 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 6 * (np.logical_and(circle_data6 < 250, circle_data6 > 0, dtype=np.float))\
+                      + np.random.uniform(0, 1, [64, 64]) * 3 * (np.logical_and(circle_data7 < 175, circle_data7 > 0, dtype=np.float))
         validation_data[j, :, :, :, 0] = np.stack((circle_data,) * 64, axis=0)
-        circle_data = np.logical_and(circle_data1 < 200, circle_data1 > 0, dtype=np.float) + np.logical_and(
-            circle_data2 < 250, circle_data2 > 0, dtype=np.float)
+        circle_data = np.logical_and(circle_data1 < 100, circle_data1 > 0, dtype=np.float) + np.logical_and(
+            circle_data4 < 125, circle_data4 > 0, dtype=np.float)
         validation_mask[j, :, :, :, 0] = np.stack((circle_data,) * 64, axis=0)
 
     epochs = 3
@@ -99,7 +137,8 @@ def testKD():
     accuracy_fn = tf.keras.metrics.binary_accuracy
 
     trainer.train(train_ds=train_ds, valid_ds=valid_ds, train_size=10, validation_size=10, loss_fn=loss_fn,
-                  accuracy_fn=accuracy_fn, BATCH_SIZE=1, EPOCHS=epochs, save_step=1)
+                  distillation_fn=tf.keras.losses.binary_crossentropy, accuracy_fn=accuracy_fn, BATCH_SIZE=1,
+                  EPOCHS=epochs, save_step=1)
 
     shutil.rmtree(model_dir)
     shutil.rmtree(model_dir + '_student_scratch')
